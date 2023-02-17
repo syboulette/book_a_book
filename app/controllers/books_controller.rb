548909e297ecc5fa6789274ffcad3bc6e5797a50
Ariane Kramer <ariane.kramer@posteo.de>
2023-02-17 10:48:13 +0100

    new book actions - destroy, update and new views - show and edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,36 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+    set_book
+  end
+
+  def edit
+    set_book
+  end
+
+  def update
+    set_book
+    if @book.update(book_params)
+      redirect_to books_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    set_book
+    @book.destroy
+    redirect_to books_path, status: :see_other
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :author, :genre, :date_of_publication, :daily_price, :language)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :date_of_publication, presence: true
   validates :author, presence: true
+  has_many :rentals
 end
