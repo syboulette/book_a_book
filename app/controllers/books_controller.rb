44104c49d517eeb97f6a4ca4115bfd4bec91be11
Dominic Zaleski <dominic.zaleski@outlook.com>
2023-02-18 15:43:11 +0100

    pull from master

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,11 @@ class BooksController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @book = Book.all
+    @books = Book.all
   end
 
   def new
     @book = Book.new
-  end
-
-  def show
-  end
-
-  def edit
   end
 
   def create
@@ -24,9 +18,36 @@ class BooksController < ApplicationController
     end
   end
 
+  def show
+    set_book
+  end
+
+  def edit
+    set_book
+  end
+
+  def update
+    set_book
+    if @book.update(book_params)
+      redirect_to books_path(@book)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    set_book
+    @book.destroy
+    redirect_to books_path, status: :see_other
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :author, :genre, :date_of_publication, :daily_price, :language)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
