05f864937d5737e1552f82f27b80234b943255f8
Sybille Lambotte <lambottesybille@gmail.com>
2023-02-21 16:16:44 +0100

    faker update

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,45 +2,43 @@ require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-<<<<<<< HEAD
-#   Character.create(name: "Luke", movie: movies.first
-=======
-#   Character.create(name: "Luke", movie: movies.first)
-
-# FAKER USER ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-# puts 'creating 20 fake users'
-# 20.times do
-#   user = User.new(
-
-#     first_name: Faker::Name.first_name,      #=> "Kaci"
-#     last_name: Faker::Name.last_name,        #=> "Ernser"
-#     address: Faker::Address.full_address, #=> "5479 William Way, East Sonnyhaven, LA 63637"
-#     email: Faker::Internet.email(domain: 'example'), #=> "alice@example.name"
-#     password: Faker::Internet.password(min_length: 6)
-#   )
-#   user.save!
-# end
-
-# user1 = User.create(email: "vinny@vinbcn.com", password: "123456")
+20.times do
+  user = User.new(
+    first_name: Faker::Name.first_name,      #=> "Kaci"
+    last_name: Faker::Name.last_name,        #=> "Ernser"
+    address: Faker::Address.full_address, #=> "5479 William Way, East Sonnyhaven, LA 63637"
+    email: Faker::Internet.email(domain: 'example'), #=> "alice@example.name"
+    password: Faker::Internet.password(min_length: 6)
+  )
+  user.save!
+end
 
 # FAKER BOOKS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-# puts "Creating 20 fake books"
-# 20.times do
-#   book = Book.new(
-#     title: Faker::Book.title, #=> "The Odd Sister"
-#     author: Faker::Book.author, #=> "Alysha Olsen"
-#     genre: Faker::Book.genre, #=> "Mystery"
-#     date_of_publication: Faker::Date.backward(days: 100_000), #=> "Fri, 19 Sep 2014"
-#     language: ["English", "Portuguese", "French", "Spanish", "German"].sample,
-#     daily_price: rand(1..2),
-#     user_id: user_1.id,
-#     status: false
-#   )
-#   book.save!
+20.times do
+  book = Book.new(
+    title: Faker::Book.title, #=> "The Odd Sister"
+    author: Faker::Book.author, #=> "Alysha Olsen"
+    genre: Faker::Book.genre, #=> "Mystery"
+    date_of_publication: Faker::Date.backward(days: 100_000), #=> "Fri, 19 Sep 2014"
+    language: ["English", "Portuguese", "French", "Spanish", "German"].sample,
+    daily_price: rand(1..2),
+    user_id: user_1.id,
+    status: false
+  )
+  book.save!
+end
+
+# Book.destroy_all
+
+# 10.times do 
+#   book = Book.create(
+#     title:Faker::Book.title
+#     author:Faker::Book.author
+#     genre: Faker::Book.genre
+#     date_of_publication: rand(1..2023)
+#     daily_price: rand(0.01..10.00)
+#     language: ["English", "Portuguese", "French", "Spanish", "German"].sample,)
+#     book.save!
 # end
->>>>>>> master
+  
