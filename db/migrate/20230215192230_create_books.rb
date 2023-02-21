948fa1fc7ba4619aeaea4b6b161a7b1f819a0303
Sybille Lambotte <lambottesybille@gmail.com>
2023-02-21 16:04:06 +0100

    faker

--- db/migrate/20230215192230_create_books.rb
+++ db/migrate/20230215192230_create_books.rb
@@ -14,3 +14,15 @@ class CreateBooks < ActiveRecord::Migration[7.0]
     end
   end
 end
+
+Book.destroy_all
+
+10.times do 
+  book.create(title:Faker::Book.unique.title
+              author:Faker::Book.unique.author
+              genre: Faker::Book.unique.genre
+              date_of_publication: rand(1..2023)
+              daily_price: rand(0.01..10.00)
+              language: Faker::Language)
+
+end
