1569710a3c7db627744f76b6dfe4194303dd7855
Vinicius Batestin <viniciusbatestin@gmail.com>
2023-02-23 23:57:59 +0100

    search bar

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem "devise"
 
 gem "pundit"
 
+gem "pg_search"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.4", ">= 7.0.4.2"
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,11 @@ class BooksController < ApplicationController
 
   def index
     @books = policy_scope(Book).all
+    if params[:query].present?
+      @books = Book.book_search(params[:query])
+    else
+      @books = Book.all
+    end
   end
 
   def new
