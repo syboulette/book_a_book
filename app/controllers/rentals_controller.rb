049af935c519c72e08408dda8a4028008550c662
Dominic Zaleski <dominic.zaleski@outlook.com>
2023-02-25 16:11:17 +0100

    removed incorrect validation, error message persisting 'book msust exist'

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -6,7 +6,7 @@ class RentalsController < ApplicationController
     @rentals = policy_scope(Rental)
     @rentals = Rental.where(user_id: current_user.id)
   end
-  
+
   def new
     @rental = Rental.new
     @book = Book.find(params[:book_id])
@@ -18,8 +18,11 @@ class RentalsController < ApplicationController
 
   def create
     @rental = Rental.new(rental_params)
-    authorize @rental
     @rental.user = current_user
+    @book = Book.find(params[:book_id])
+
+    authorize @rental
+
     if @rental.save!
       redirect_to rental_path(@rental)
     else
@@ -27,13 +30,11 @@ class RentalsController < ApplicationController
     end
   end
 
-
-
   def destroy
     @rental.destroy
     redirect_to root_path, status: :see_other, notice: "Your booking has been removed"
   end
-  
+
   private
 
   def rental_params
