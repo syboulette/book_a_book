13c8f4a41ad2d8e8e57ac8b2484eb4828205d934
Ariane Kramer <ariane.kramer@posteo.de>
2023-02-25 10:55:23 +0100

    nothing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_22_154316) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_24_184326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,13 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_22_154316) do
     t.string "title"
     t.string "author"
     t.string "genre"
-    t.integer "date_of_publication"
+    t.date "date_of_publication"
     t.integer "daily_price"
     t.string "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "status"
     t.bigint "user_id", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
